6f955e6021a47d608c6ddee4319472b089b91afd
Markus Ackermann <saigyo@kaixo.de>
2014-04-29 07:16:01 +0200

    removed numbering of rows

--- texify-radicals.sh
+++ texify-radicals.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -v
-tr "\t" "&" < 100-most-common-radicals.txt | gsed -E -e s/$/\\\\nl/ -e s/^/"\\\\nr\\&"/ > 100-most-common-radicals.inc
+tr "\t" "&" < 100-most-common-radicals.txt | gsed -E -e s/$/\\\\nl/ > 100-most-common-radicals.inc
